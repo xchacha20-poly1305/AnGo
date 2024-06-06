d295e577c3e1b329d4fe08c80e72e7bcc897cf1e
HystericalDragon <HystericalDragons@proton.me>
2024-06-06 10:15:02 +0800

    Fix GOBIN

--- cmd/ango/cmp.go
+++ cmd/ango/cmp.go
@@ -11,8 +11,17 @@ func goBins() []string {
 	binFromEnv := cmp.Or(
 		pathGoBin,
 		os.Getenv("GOBIN"),
-		filepath.Join(os.Getenv("GOPATH"), "bin"),
+		gopathBin(),
 		"~/go/bin",
 	)
+	binFromEnv = os.ExpandEnv(binFromEnv)
 	return filepath.SplitList(binFromEnv)
+}
+
+func gopathBin() string {
+	gopath, found := os.LookupEnv("GOPATH")
+	if !found {
+		return ""
+	}
+	return filepath.Join(gopath, "bin")
 }
